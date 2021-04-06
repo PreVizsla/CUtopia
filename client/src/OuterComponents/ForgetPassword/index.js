4fc2b4ff5964872dfe1d7eb9f59b0a36ed2e08c8
PreVizsla <Leonardokwan1@yahoo.com>
2021-04-06 12:57:19 +0700

    Deleted some legacy comments

--- client/src/OuterComponents/ForgetPassword/index.js
+++ client/src/OuterComponents/ForgetPassword/index.js
@@ -47,39 +47,12 @@ const ForgetPasswordForm = ({submitFormForget}) => {
                
                 <BtnWrapper>
                     <NextBtn to='./signup_signin' >back</NextBtn>
-                    {/* <Submit_btn type="submit" >
-                        <Link_text to='./next' >
-                            next
-                        </Link_text>
-                    </Submit_btn> */}
                     <SubmitBtn type="submit" >
                     Login
                     </SubmitBtn>          
                 </BtnWrapper> 
             </form>
         </Form>
-        // <form>
-        //     <Form>
-        //     <Heading>Forget Password</Heading>
-        //     <Content>Enter your Email Address</Content>
-        //     <Content>(xxxxxxxxxx@link.cuhk.edu.hk)</Content>
-            
-		// 		<EmailField type="email"  placeholder="Email Id" pattern=".+@link.cuhk.edu.hk"  required />
-        //     </Form>
-        //     <BtnWrapper>
-        //         <NextBtn to='./signup_signin' >back</NextBtn>
-        //         {/* <Submit_btn type="submit" >
-        //             <Link_text to='./next' >
-        //                 next
-        //             </Link_text>
-        //         </Submit_btn> */}
-
-        //         <NextBtn to='./next' >next</NextBtn>
-
-            
-        //     </BtnWrapper>
-            
-        // </form>
     )
 }
 
