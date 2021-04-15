51aa5e72937b9abc4deea77eedf5f627f5e539a3
itsadityavarshney <68473088+itsadityavarshney@users.noreply.github.com>
2021-04-15 17:33:27 +0530

    Update middleware.js

--- prototype/middleware.js
+++ prototype/middleware.js
@@ -1,3 +1,5 @@
+// to facilitate the opening of different pages on the client side and display certain pages only 
+//after having successfully authenicated the user's credentials as stored in our databse.
 exports.requireLogin = (req, res, next) => {
     if (req.session && req.session.user) {
         return next();
